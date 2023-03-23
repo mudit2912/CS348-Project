e9bf3609b268cb15480c617620e2cee37b722b34
zm-codes <90979781+zm-codes@users.noreply.github.com>
2023-03-22 20:15:17 -0400

    Add files via upload

---
+++ milestone_2/C5/C5/frontend/src/components/index.js
@@ -0,0 +1,11 @@
+// Exports
+export { default as Home } from "./Home/Controller";
+export { default as Login } from "./Auth/Login";
+export { default as SignUp } from "./Auth/SignUp";
+export { default as ViewProfile } from "./Profile/ViewProfile";
+export { default as GlobalLeaderboard } from "./Leaderboard/Global";
+export { default as NationalLeaderboard } from "./Leaderboard/National";
+export { default as HeadToHead } from "./H2H/Controller";
+
+// Place new component exports below, if they are used in the Router
+// i.e. If they should be accessed by a URL
