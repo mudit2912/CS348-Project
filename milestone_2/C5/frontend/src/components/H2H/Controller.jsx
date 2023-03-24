deecbc247c29b4e8f7b3285dc81e5fa43c81f13e
zm-codes <90979781+zm-codes@users.noreply.github.com>
2023-03-24 01:07:14 -0400

    Add files via upload

--- milestone_2/C5/frontend/src/components/H2H/Controller.jsx
+++ milestone_2/C5/frontend/src/components/H2H/Controller.jsx
@@ -10,19 +10,32 @@ function HeadToHead(props) {
     const [showingPage, setShowingPage] = useState(0);
     const [lifterA, setLifterA] = useState("");
     const [lifterB, setLifterB] = useState("");
-    const [comparisonData, setComparisonData] = useState(null);
+    const [lifterUser_A, setLifterUser_A] = useState(null);
+    const [lifterUser_B, setLifterUser_B] = useState(null);
+    const [lifterData_A, setLifterData_A] = useState(null);
+    const [lifterData_B, setLifterData_B] = useState(null);
 
     async function loadComparison() {
-        
         const response = await getHeadToHeadComparison(lifterA, lifterB);
-
-        /*
-        if (!response.ok) {
-            alert("Oops! There was an error comparing the lifters.");
+        if (response.status !== 200) {
+            if (response.status === 404) alert("Oops! One of the lifters was not found.");
+            else alert("Oops! There was an error comparing the lifters.");
             return;
         }
-        */
-        setComparisonData({});
+        
+        var userA = response.data.users[0]; var userB = response.data.users[1];
+
+        var resultsA = []; var resultsB = [];
+        for (const [key, value] in Object.entries(response.data.lifts)) {
+            const curr_meet = response.data.lifts[key];
+            if (curr_meet.id === userA.id) resultsA.push(curr_meet);
+            else resultsB.push(curr_meet);
+        }
+
+        setLifterUser_A(userA);
+        setLifterUser_B(userB);
+        setLifterData_A(resultsA);
+        setLifterData_B(resultsB);
         setShowingPage(1);
     }
 
@@ -33,6 +46,54 @@ function HeadToHead(props) {
             return;
         }
         loadComparison();
+    }
+
+    function renderLifts(lifterData) {
+        var r = [];
+        lifterData.forEach(x => {
+            r.push(
+                <div className="home feed post">
+                    <div className="top">
+                        <h1>{x.meet_name}</h1>
+                        <h2>{x.meet_country}</h2>
+                        <h2>{x.meet_date}</h2>
+                    </div>
+                    <div className="bottom">
+                        <div className="lifts">
+                            <div>
+                                <h3>Bench (kg)</h3>
+                                <h1>{x.best3benchkg}</h1>
+                            </div>
+                            <div>
+                                <h3>Squat (kg)</h3>
+                                <h1>{x.best3squatkg}</h1>
+                            </div>
+                            <div>
+                                <h3>Deadlift (kg)</h3>
+                                <h1>{x.best3deadliftkg}</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <div className="lifts">
+                            <div>
+                                <h3>IPFP Points</h3>
+                                <h1>{x.ipfp_points}</h1>
+                            </div>
+                            <div>
+                                <h3>Glossbrenner</h3>
+                                <h1>{x.glossbrenner}</h1>
+                            </div>
+                            <div>
+                                <h3>McCullough</h3>
+                                <h1>{x.mccullough}</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+        return r;
     }
 
     return (
@@ -49,9 +110,21 @@ function HeadToHead(props) {
             </form>
             }
             {(showingPage === 1) &&
-                <div className="h2h comparison">
-                    <div>
-                        {}
+                <div>
+                    <button onClick={()=>{setShowingPage(0);}}>Go Back</button>
+                    <div className="h2h comparison">
+                        <div>
+                            <img src={lifterUser_A.pfp_url} />
+                            <h1>{lifterUser_A.username}</h1>
+                            <h3>{lifterUser_A.bio}</h3>
+                            { renderLifts(lifterData_A) }
+                        </div>
+                        <div>
+                            <img src={lifterUser_B.pfp_url} />
+                            <h1>{lifterUser_B.username}</h1>
+                            <h3>{lifterUser_B.bio}</h3>
+                            { renderLifts(lifterData_B) }
+                        </div>
                     </div>
                 </div>
             }
